e414053521fbabd8f786637671342b3e2e89d13a
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-26 23:01:38 +0300

    add task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" python file with
+    auth class and functions
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth():
+    """ auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ require function
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ auth header
+        """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ current user
+        """
+        return request
