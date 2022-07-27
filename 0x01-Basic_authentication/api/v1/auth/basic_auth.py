75e68ae6c249082d22ff1e268496d49176160500
f-i-t-s-u-m <fibabirr@gmail.com>
2022-07-27 11:12:29 +0300

    add task 6

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" basic auth python file
+    with basic_auth class
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ basic auth class
+    """
