d02a9cd166bb3fe2911fe058f0a3d51f26e48678
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-03 23:49:08 +0300

    add task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -56,9 +56,13 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """ update user data
         """
-        if kwargs is None or type(user_id) is not int:
-            raise ValueError
+
         user = self.find_user_by(id=user_id)
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise ValueError
 
         for k, v in kwargs.items():
             setattr(user, k, v)
