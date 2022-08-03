0c8792632e79f50a6edb55ec2cd01fa3a89478db
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-03 10:54:43 +0300

    add nullable to id

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -12,7 +12,7 @@ class User(Base):
     """ user model """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False)
     hashed_password = Column(String, nullable=False)
     session_id = Column(String, nullable=True)
