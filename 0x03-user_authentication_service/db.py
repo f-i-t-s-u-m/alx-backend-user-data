a8a02d42ffc4b31391b646145a2c253a70794dc5
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-03 23:40:51 +0300

    add task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,7 +53,7 @@ class DB:
             raise NoResultFound
         return user
 
-    def update_user(self, user_id, **kwargs) -> None:
+    def update_user(self, user_id: int, **kwargs) -> None:
         """ update user data
         """
 
