15b3ef3a530ce0adbc94acae3a6616aefaddb023
f-i-t-s-u-m <fibabirr@gmail.com>
2022-08-03 11:25:04 +0300

    change name

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,10 +32,10 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ add user function
         """
-        user = User(email=email, hashed_password=password)
+        user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
